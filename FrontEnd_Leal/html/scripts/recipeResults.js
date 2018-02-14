3d0b6bb3fdfce36ad81c582b3c79d26121a07e09
Chris Leal <leal@b98-aruba1-guest-10-110-48-164.wlan.cpp.edu>
2018-02-13 16:35:08 -0800

    Added new recipes
    pretty sure i added 15 or so...

--- FrontEnd_Leal/html/scripts/recipeResults.js
+++ FrontEnd_Leal/html/scripts/recipeResults.js
@@ -5,12 +5,14 @@
     
 function open_close(){
     if(c % 2 == 0){    
-        document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";  
+        this.document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";
         c++;
     }else {
-        document.querySelector('.cont_modal').className = "cont_modal";  
-        c++;    
-    }  
+        this.document.querySelector('.cont_modal').className = "cont_modal";  
+        c++;   
+    } 
+    
+    
 } 
 
 function setRecipeImage(newSrc){

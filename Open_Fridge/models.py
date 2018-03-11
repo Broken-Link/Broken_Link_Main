fc8a50e4c67bcb7617aff547022b0b90089fcc46
fatcatmat <32988747+fatcatmat@users.noreply.github.com>
2018-03-10 18:24:52 -0800

    Update models.py

--- Open_Fridge/models.py
+++ Open_Fridge/models.py
@@ -69,7 +69,7 @@ class Recipe(models.Model):
     name = models.CharField(db_column='Name', max_length=45)  # Field name made lowercase.
     steps = models.TextField(db_column='Steps')  # Field name made lowercase.
     username = models.CharField(max_length=150)
-    image = models.ImageField(upload_to = 'static/images/')
+    image = models.ImageField(upload_to = '/static/images/')
     class Meta:
         managed = False
         db_table = 'RECIPE'

53bbd599ad250c6c29227d6cad0dd6c321f9b472
gutierrez0103 <erik.gutierrez0103@gmail.com>
2018-03-11 23:26:47 -0700

    Small changes to 720p User Account Page

--- Open_Fridge/models.py
+++ Open_Fridge/models.py
@@ -69,7 +69,11 @@ class Recipe(models.Model):
     name = models.CharField(db_column='Name', max_length=45)  # Field name made lowercase.
     steps = models.TextField(db_column='Steps')  # Field name made lowercase.
     username = models.CharField(max_length=150)
-    image = models.ImageField(upload_to = '/opt/fridge480/fridge480/static/images/')
+    # Runs for Matt
+    # image = models.ImageField(upload_to = '/opt/fridge480/fridge480/static/images/')
+    
+    # Runs for Erik
+    image = models.ImageField(upload_to = '../static/images/')
     likes = models.IntegerField()
     class Meta:
         managed = False

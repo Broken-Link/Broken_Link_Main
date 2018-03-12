79f6e387c76175aaa2ab030bd03d90eacad30c9d
Pedro Ruelas <pruelas@cpp.edu>
2018-03-11 23:47:30 -0700

    Update urls.py

--- fridge480/urls.py
+++ fridge480/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('index/', include('Open_Fridge.urls')),
     path('', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='/index/')),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
